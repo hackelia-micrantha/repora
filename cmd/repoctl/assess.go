2675618444ee20b303022475e78f8f954dbd2daf
RJ <rybertjen@gmail.com>
2026-08-11 23:32:59 -0700

    Add bounded assessment skeleton creation

---
+++ cmd/repoctl/assess.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
+	"repoctl/internal/assessment"
+)
+
+func runAssess(args []string) int {
+	if len(args) == 1 && (args[0] == "-h" || args[0] == "--help") {
+		fmt.Fprintln(os.Stdout, "usage: repoctl assess FILE")
+		return 0
+	}
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, "usage: repoctl assess FILE")
+		return 1
+	}
+
+	report := assessment.NewSkeleton()
+	if err := report.Validate(); err != nil {
+		fmt.Fprintf(os.Stderr, "repoctl: generated assessment template is invalid: %v\n", err)
+		return 1
+	}
+	data, err := json.MarshalIndent(report, "", "  ")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "repoctl: encode assessment template: %v\n", err)
+		return 1
+	}
+	data = append(data, '\n')
+
+	if err := writeNewAssessmentFile(args[0], data); err != nil {
+		fmt.Fprintf(os.Stderr, "repoctl: %v\n", err)
+		return 1
+	}
+	fmt.Fprintf(os.Stdout, "created assessment template %s\n", args[0])
+	return 0
+}
+
+func writeNewAssessmentFile(path string, data []byte) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o644)
+	if err != nil {
+		if errors.Is(err, os.ErrExist) {
+			return fmt.Errorf("assessment report already exists: %s", path)
+		}
+		return fmt.Errorf("create assessment report: %w", err)
+	}
+
+	cleanup := true
+	defer func() {
+		if cleanup {
+			_ = file.Close()
+			_ = os.Remove(path)
+		}
+	}()
+
+	written, err := file.Write(data)
+	if err != nil {
+		return fmt.Errorf("write assessment report: %w", err)
+	}
+	if written != len(data) {
+		return fmt.Errorf("write assessment report: short write %d of %d bytes", written, len(data))
+	}
+	if err := file.Close(); err != nil {
+		return fmt.Errorf("close assessment report: %w", err)
+	}
+	cleanup = false
+	return nil
+}
